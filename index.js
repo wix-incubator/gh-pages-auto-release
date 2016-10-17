f3a1c911fd910e69a4895578ad01b4306a4cd715
Shahar Talmi <shahar.talmi@gmail.com>
2016-10-17 20:54:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+var commandLineArgs = require('command-line-args');
 var workingDir = process.cwd();
+
+var options = commandLineArgs([
+  {name: 'dist', defaultValue: 'dist'}
+]);
+
 require('child_process')
-  .spawn(__dirname + '/release.sh', [workingDir], {stdio: 'inherit'});
+  .spawn(__dirname + '/release.sh', [workingDir, options.dist], {stdio: 'inherit'});
