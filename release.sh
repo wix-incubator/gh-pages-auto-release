3efdb6f51202cc48544f95b9b112e5f133d73643
Shahar Talmi <shahar.talmi@gmail.com>
2017-04-14 12:36:48 +0300

    purging gh-pages history with each release

--- release.sh
+++ release.sh
@@ -4,13 +4,11 @@ cd /tmp
 rm -rf gh-pages
 git clone $GIT_REMOTE_URL gh-pages
 cd gh-pages
-git checkout gh-pages
-git checkout -b gh-pages
-git push origin gh-pages
+git checkout --orphan gh-pages
 shopt -s extglob dotglob
 
 rm -rf !(.git|.|..)
 cp -R $1/$2/* .
 git add --all .
 git commit -a -m "gh-pages"
-git push --set-upstream origin gh-pages
+git push --set-upstream --force origin gh-pages
