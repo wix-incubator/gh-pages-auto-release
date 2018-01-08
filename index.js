98bf184546a66c16aa52a37aa9dff31fd7d5537c
David Perlman <davidp@wix.com>
2018-01-08 11:05:22 +0200

    added git-repo option for monoreps needing more than one gh-pages (#1)

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var commandLineArgs = require('command-line-args');
 var workingDir = process.cwd();
 
 var options = commandLineArgs([
-  {name: 'dist', defaultValue: 'dist'}
+  {name: 'dist', defaultValue: 'dist'},
+  {name: 'git-repo', defaultValue: process.env.GIT_REMOTE_URL},
 ]);
 
 require('child_process')
-  .spawn(__dirname + '/release.sh', [workingDir, options.dist], {stdio: 'inherit'});
+  .spawn(__dirname + '/release.sh', [workingDir, options.dist, options['git-repo']], {stdio: 'inherit'});
